59de2858e5972ecf3d3c747d1a70a98ac29bccd3
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-08 17:14:33 -0300

    Colocando a função (enviar dados)

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,17 +1,34 @@
 import { FaUser, FaLock } from "react-icons/fa";
 import { useState } from "react";
+import "./Login.css";
+
 
 const Login = () => {
+
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+const handleSubmit = (event) => {
+    event.preventDeFault();
+    console.log("Envio");
+    
+    alert("Enviando os dados:" + username + " - " + password);
+};
+
     return (
         <div className="container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Faça Login</h1>
                 <div>
-                    <input type="email" placeholder="E-mail" />
+                    <input type="email"
+                     placeholder="E-mail"
+                     required
+                     onChange={(e) => setUsername(e.target.value)}  />
                     <FaUser className="icon" />
                 </div>
                 <div>
-                    <input type="password" placeholder="Senha" />
+                    <input type="password" placeholder="Senha"
+                    onChange={(e) => setPassword(e.target.value)} />
                     <FaLock className="icon" />
                 </div>
 
