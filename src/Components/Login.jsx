9a63c365783b9f6be7472e9647fe03acb94862e0
CamillaCarvalho <Camillacarvalho037@gmail.com>
2024-07-09 16:16:16 -0300

    Corrigindo um erro no link (Registrar)

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -43,7 +43,8 @@ const handleSubmit = (event) => {
                 <button>Entrar</button>
 
                 <div className="signup-link">
-                    <p>Não tem uma conta? <a className="registrer" href="#"></a>Registrar</p>
+                    <p>Não tem uma conta?
+                         <a href="#">Registrar</a></p>
                 </div>
 
             </form>
